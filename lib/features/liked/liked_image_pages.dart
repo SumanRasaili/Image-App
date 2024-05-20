5f7c935b299f2c3d48096d78efce753c95d9fb8a
SumanRasaili <rasailisuman123@gmail.com>
2024-05-21 01:24:54 +0545

    isar for local

--- lib/features/liked/liked_image_pages.dart
+++ lib/features/liked/liked_image_pages.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vritapp/features/liked/services/isar_local_services.dart';
 
 class LikedImagesScreen extends ConsumerWidget {
   const LikedImagesScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final data = ref.watch(localPhotosServiceProvider);
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
         title: const Text("Liked Images"),
       ),
-      body: const Center(
-        child: Text("Liked images not found!"),
-      ),
+      body: Center(
+          child: Column(
+        children: [
+          FutureBuilder(
+            future: data.getAllLikedPhotos(),
+            builder: (context, snapshot) {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  return Text(snapshot.data?[index].title ?? "");
+                },
+              );
+            },
+          )
+        ],
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:vritapp/core/model/local_isar_photos_model.dart';
 import 'package:vritapp/core/notification_service/notification_services.dart';
 import 'package:vritapp/firebase_options.dart';
 import 'package:vritapp/splash_screen.dart';
 
+late Isar isar;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Notificationservice().initialize();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  runApp( ProviderScope(child: Phoenix(child: const MyApp())));
+  final dir = await getApplicationDocumentsDirectory();
+  isar = await Isar.open(
+    [PhotosCollectionSchema],
+    directory: dir.path,
+  );
+  runApp(ProviderScope(child: Phoenix(child: const MyApp())));
 }
 
 class MyApp extends ConsumerWidget {
-
   const MyApp({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
